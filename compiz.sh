d8d8615e4a16240b3d490057139fdb9074f9a421
Achmad Mahardi <maman.lalilu@gmail.com>
2012-06-07 06:00:07 +0700

    major update

--- compiz.sh
+++ compiz.sh
@@ -1,21 +1,36 @@
 #!/bin/bash
 
+#start gnome-polkit auth
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+
 #Merging .Xdefaults
 xrdb -merge ~/.Xdefaults &
 
 #set X font path
-xset +fp /home/kitty/.fonts &
-xset +fp /usr/share/fonts/jmk &
+xset +fp /home/derp/.fonts &
+xset +fp /usr/share/fonts/X11/jmk &
+#xset +fp /home/derp/.fonts-2 &
 xset fp rehash &
 
 #network
-wicd-client &
+#wicd-client &
+nm-applet &
+dropboxd &
 
 #urxvt daemon
 urxvtd &
 
 #thunar daemon
 thunar --daemon &
+
+#automount daemon
+udiskie &
+
+#starting dropbox
+#pidof dropbox >& /dev/null
+#if [ $? -ne 0 ]; then
+#  dropbox start &
+#fi
 
 #starting mpd & last.fm daemon
 pidof mpd >& /dev/null
@@ -27,24 +42,35 @@ if [ $? -ne 0 ]; then
   mpdscribble &
 fi
 
-#enable two-finger scrolling
-sh /home/kitty/dots/touchpad.sh &
+#enable two-finger reverse scrolling
+xinput set-button-map 12 1 2 3 5 4
+
+#enable custom Xorg keybinds
+#xmodmap /home/derp/.Xmodmap
 
 #enable notification daemon
-pidof notification-daemon >& /dev/null
-if [ $? -ne 0 ]; then
-  /usr/lib/notification-daemon-1.0/notification-daemon &
-fi
+#pidof notification-daemon >& /dev/null
+#if [ $? -ne 0 ]; then
+  #/usr/lib/notification-daemon-1.0/notification-daemon &
+  #twmnd &
+#fi
+
+#dzens
+/home/derp/.config/dzen/time &
+/home/derp/.config/dzen/mpd &
 
 #coooonkies
-conky -c /home/kitty/git/conkyconf/conkyrc2 &
-conky -c /home/kitty/git/conkyconf/conkyrc3 &
+#conky -c /home/kitty/git/conkyconf/conkyrc2 &
+#conky -c /home/kitty/git/conkyconf/conkyrc3 &
 
 #wallpaper - compiz wallpaper plugin have issues with dualmonitor
 nitrogen --restore &
 
-#exec bmpanel2
-#bmpanel2 --theme=amana &
+#exec stalonetray
+stalonetray &
+
+#exec AWN
+avant-window-navigator &
 
 #exec Compiz
 compiz ccp
--- sawfish.sh
+++ sawfish.sh
@@ -1,21 +1,41 @@
 #!/bin/bash
 
+#start gnome-polkit auth
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+
 #Merging .Xdefaults
 xrdb -merge ~/.Xdefaults &
 
 #set X font path
-xset +fp /home/kitty/.fonts &
-xset +fp /usr/share/fonts/jmk &
+xset +fp /home/derp/.fonts &
+#xset +fp /home/derp/.fonts-2 &
 xset fp rehash &
 
 #network
-wicd-client &
+#wicd-client &
+nm-applet &
+tidybattery &
+volumeicon &
+dropboxd &
 
 #urxvt daemon
 urxvtd &
 
-#thunar daemon
-thunar --daemon &
+#devmon daemon
+#devmon &
+
+#udisksvm daemon
+if ! pgrep -lf "udisks-daemon: polling /dev/sr0"; then
+    udisks --poll-for-media /dev/sr0
+fi
+
+udisksvm >/dev/null &
+
+#starting dropbox
+#pidof dropbox >& /dev/null
+#if [ $? -ne 0 ]; then
+#  dropbox start &
+#fi
 
 #starting mpd & last.fm daemon
 pidof mpd >& /dev/null
@@ -27,19 +47,34 @@ if [ $? -ne 0 ]; then
   mpdscribble &
 fi
 
-#enable touchpad enhancements
-xinput set-button-map 11 1 2 3 5 4
+#enable two-finger reverse scrolling
+xinput set-button-map 12 1 2 3 5 4
 
-#wallpaper
+#enable custom Xorg keybinds
+#xmodmap /home/derp/.Xmodmap
+
+#enable notification daemon
+#pidof notification-daemon >& /dev/null
+#if [ $? -ne 0 ]; then
+  #/usr/lib/notification-daemon-1.0/notification-daemon &
+#  twmnd &
+#fi
+
+#dzens
+#/home/derp/.config/dzen/time &
+#/home/derp/.config/dzen/mpd &
+
+#compositing engine
+compton -o 0.6 -r 20 -l -30 -t -10 -cCGb &
+
+#idle monitor
+xautolock -time 30 -locker "sudo pm-suspend" &
+
+#wallpaper - compiz wallpaper plugin have issues with dualmonitor
 nitrogen --restore &
 
-#notification daemon
-/usr/lib/notification-daemon-1.0/notification-daemon &
+#exec bmpanel2
+bmpanel2 --theme=darks &
 
-#taskbar and system notifications yay
-bmpanel2 &
-conky -c ~/git/conkyconf/conkyrc4 &
-conky -c ~/git/conkyconf/conkyrc5 &
-
-#exec Compiz
+#exec Sawfish
 sawfish
